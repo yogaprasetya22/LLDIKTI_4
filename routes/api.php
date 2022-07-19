4cddce2f28eee10fd62bac8f624ebd01a1ca9bd9
tecmaniac <59366742+tecmaniac@users.noreply.github.com>
2022-07-19 21:32:51 +0700

    1. Update Table naming convention 2. Api Formatter 3. Api News

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\NewsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/News', [NewsController::class, 'index']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
