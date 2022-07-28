279b150c2dc3b2e9d6cc56a84ef18b00a8929bdd
yoga <yogaagle123z@gmail.com>
2022-07-28 07:27:02 +0700

    new

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,7 @@
         <title inertia>{{ config('app.name', 'LLDIKTI Wilayah 4') }}</title>
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        {{-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap"> --}}
 
         <!-- Scripts -->
         @routes
