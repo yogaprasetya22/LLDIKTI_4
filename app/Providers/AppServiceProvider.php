27c42ddba4dee9c6771d21a11e36911d466e7c25
yoga <yogaagle123z@gmail.com>
2022-07-28 17:13:38 +0700

    new

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,11 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // if (env('APP_ENV') !== 'local') {
-        //     $this->app['request']->server->set('HTTPS', true);
-        // }
-
-        // Schema::defaultStringLength(191);
 
         if (env('APP_ENV') !== 'local') {
             URL::forceScheme('https');
